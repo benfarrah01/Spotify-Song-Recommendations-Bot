129fcd35db6c4d09774428eb4b9be6e3375f79c4
Payton Mineweaser <mineweaserp@allegheny.edu>
2021-11-18 15:41:34 -0500

    Country and Pop

--- src/Country.py
+++ src/Country.py
@@ -1,2 +1,16 @@
+import os
+import spotify.sync as spotify
+
+CLIENT = os.getenv('FF_SPOTIFY_CLIENT')
+SECRET = os.getenv('FF_SPOTIFY_CLIENT_SECRET')
+
+client = spotify.Client(CLIENT, SECRET)
+result = client.search('Hot Country', types=['playlist'])
+
+for playlist in result.playlists:
+    print(playlist)
+
+client.close()
+
 # This is the Country Playlist File 
 # This will get the a random song from the Hot Country playlist in Spotify
