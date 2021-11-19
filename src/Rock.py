3e2217774ad752850059894738a88e049240dbf8
benfarrah01 <farrah01@allegheny.edu>
2021-11-19 09:59:46 -0500

    Trying to make a single playlist class instead of a bunch of files for each genre.

--- src/Rock.py
+++ src/Rock.py
@@ -2,6 +2,7 @@
 # This will get the a random song from the Rock This playlist in Spotify
 
 import os
+import random
 import spotify.sync as spotify
 
 CLIENT = os.getenv('FF_SPOTIFY_CLIENT')
@@ -10,10 +11,9 @@ SECRET = os.getenv('FF_SPOTIFY_CLIENT_SECRET')
 client = spotify.Client(CLIENT, SECRET)
 result = client.search('Rock This', types=['playlist'])
 
-# for playlist in result.playlists:
-#     print(playlist)
-
-tracks = get_all_tracks()
-print(tracks)
+for playlist in result.playlists:
+    tracks = playlist.get_all_tracks()
+    track = random.choice(tracks)
+    print(f"My mixtape has the song {track.name} by {track.artist.name}. Hell yeah.")
     
 client.close()
