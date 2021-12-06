358a00693b675b7967ae73be84a6cea148f2a62e
dluman <dluman@allegheny.edu>
2021-12-06 09:44:38 -0500

    Adding a Song object to make things harder

--- src/Genre.py
+++ src/Genre.py
@@ -2,6 +2,7 @@ import os
 import random
 import json
 import spotify.sync as spotify
+from Song import Song
 
 
 def load_file(filename):
@@ -15,25 +16,24 @@ def pick_one(data):
 def get_song():
     genre = load_file("data/genres.json")
     genre = genre["genres"]
-    
     genre = pick_one(genre)
-    print(genre)
+    #print(genre)
 
     CLIENT = os.getenv('FF_SPOTIFY_CLIENT')
     SECRET = os.getenv('FF_SPOTIFY_CLIENT_SECRET')
 
     client = spotify.Client(CLIENT, SECRET)
     result = client.search(genre, types=['playlist'],limit=5)
-    
-    songs = {}
+
+    songs = []
     for playlist in result.playlists:
         tracks = playlist.get_all_tracks()
         track = random.choice(tracks)
-        songs[f'{track.name}'] = track.popularity
-    print(songs)
-    song = max(songs, key=songs.get)
+        songs.append(Song(track).song_as_dict())
+
+    print(song)
     client.close()
-    return song
+    #return song
 
 
     
